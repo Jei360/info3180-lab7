2f3d7718142d071846d7fb41f3c2ee429f5cf476
Jovan Dalhouse <jovan_360@hotmail.com>
2019-03-25 18:45:45 +0000

    First- Form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import InputRequired, DataRequired, Email
 
+class UploadForm(FlaskForm):
+    description= StringField('description', validators= [InputRequired()])
+    photo= FileField('photo', validators= [DataRequired(), FileAllowed(['jpg', 'png'])])
