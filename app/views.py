bef5fc75a1710d9ccc021f21bead2b6c88b0aeac
Jovan Dalhouse <jovan_360@hotmail.com>
2019-03-25 20:21:30 +0000

    Second- Validate

--- app/views.py
+++ app/views.py
@@ -4,14 +4,32 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import os
 from app import app
 from flask import render_template, request
+from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 
 ###
 # Routing for your application.
 ###
-
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    myform= UploadForm() 
+    if myform.validate_on_submit():
+        desc=myform.description.data
+        photo= request.file['photo']
+        filename= secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        return {
+            "message": "File Upload Successful",
+            "photoname": filename,
+            "description": desc
+        }
+    else:
+        return {
+            "errors": form_errors(myform)
+        }
 
 # Please create all new routes and view functions above this route.
 # This route is now our catch all route for our VueJS single page
